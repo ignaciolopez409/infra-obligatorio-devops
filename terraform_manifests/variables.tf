4dc08d9bc83892f00c29e4f34378210dfbe4364c
ilopez <ilopez@geocom.com.uy>
2021-11-14 23:32:32 -0300

    develop arn policies

--- terraform_manifests/variables.tf
+++ terraform_manifests/variables.tf
@@ -4,18 +4,18 @@ variable "ENV" {
   type = string
 }
 
-variable "orders_service_version" {
-  type = string
-}
-variable "products_service_version" {
-  type = string
-}
-variable "shipping_service_version" {
-  type = string
-}
-variable "payments_service_version" {
-  type = string
-}
+//variable "orders_service_version" {
+//  type = string
+//}
+//variable "products_service_version" {
+//  type = string
+//}
+//variable "shipping_service_version" {
+//  type = string
+//}
+//variable "payments_service_version" {
+//  type = string
+//}
 
 variable "region" {
   type = string
