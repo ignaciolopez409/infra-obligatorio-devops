9e1a71f3f36b100ead7b57865e2a8e8a15a0be03
ilopez <ilopez@geocom.com.uy>
2021-11-15 00:40:13 -0300

    instance_type list(string)

--- terraform_manifests/aws_roles.tf
+++ terraform_manifests/aws_roles.tf
@@ -6,13 +6,13 @@ data "aws_iam_policy" "AmazonNodePolicy" {
   arn = var.arn_worker_node_policy
 }
 
-//data "aws_iam_policy" "AmazonEC2CR_RO" {
-//  arn = var.arn_ec2_cr_ro
-//}
-//
-//data "aws_iam_policy" "AmazonCNI" {
-//  arn = var.arn_cni_policy
-//}
+data "aws_iam_policy" "AmazonEC2CR_RO" {
+  arn = var.arn_ec2_cr_ro
+}
+
+data "aws_iam_policy" "AmazonCNI" {
+  arn = var.arn_cni_policy
+}
 
 resource "aws_iam_role" "eksClusterRole" {
   name = "eksClusterRole"
@@ -24,8 +24,12 @@ resource "aws_iam_role" "eksWorkerRole" {
   assume_role_policy = data.aws_iam_policy.AmazonNodePolicy.policy
 }
 
+resource "aws_iam_role_policy_attachment" "EC2_Policy" {
+  policy_arn = data.aws_iam_policy.AmazonEC2CR_RO.arn
+  role = aws_iam_role.eksClusterRole.id
+}
 
-resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
-  role = aws_iam_role.eksWorkerRole.name
-  policy_arn = [var.arn_ec2_cr_ro, var.arn_cni_policy]
+resource "aws_iam_role_policy_attachment" "CNI_Policy" {
+  policy_arn = data.aws_iam_policy.AmazonCNI.arn
+  role = aws_iam_role.eksClusterRole.id
 }
