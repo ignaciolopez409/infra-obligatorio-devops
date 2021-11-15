7c57428de32e8256071e4b9c78aa829e227d70f8
ilopez <ilopez@geocom.com.uy>
2021-11-14 23:55:53 -0300

    instance_type list(string)

--- terraform_manifests/aws_roles.tf
+++ terraform_manifests/aws_roles.tf
@@ -4,16 +4,16 @@ data "aws_iam_policy" "AmazonEKSClusterPolicy" {
 
 resource "aws_iam_role" "eksClusterRole" {
   name = "eksClusterRole"
-  assume_role_policy = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
+  assume_role_policy = data.aws_iam_policy.AmazonEKSClusterPolicy.policy
 }
 
 resource "aws_iam_role" "eksWorkerRole" {
   name = "eksWorkerRole"
-  assume_role_policy = var.arn_worker_policy_list[0]
+  assume_role_policy = var.arn_worker_policy_list[0].policy
 }
 
 resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
   role = aws_iam_role.eksWorkerRole.name
   count = length(var.arn_worker_policy_list)
-  policy_arn = var.arn_worker_policy_list[count.index]
+  policy_arn = var.arn_worker_policy_list[count.index].policy
 }
