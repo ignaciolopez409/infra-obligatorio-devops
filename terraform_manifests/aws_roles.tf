842e788fa4400f4a95d3eb8a684b80382cb79557
ilopez <ilopez@geocom.com.uy>
2021-11-15 21:24:17 -0300

    test policy

--- terraform_manifests/aws_roles.tf
+++ terraform_manifests/aws_roles.tf
@@ -16,188 +16,56 @@ data "aws_iam_policy" "AmazonCNI" {
 
 resource "aws_iam_role" "eksClusterRole" {
   name = "eksClusterRole"
-  assume_role_policy = jsonencode({
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Effect: "Allow",
-        Action: [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:UpdateAutoScalingGroup",
-          "ec2:AttachVolume",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:CreateRoute",
-          "ec2:CreateSecurityGroup",
-          "ec2:CreateTags",
-          "ec2:CreateVolume",
-          "ec2:DeleteRoute",
-          "ec2:DeleteSecurityGroup",
-          "ec2:DeleteVolume",
-          "ec2:DescribeInstances",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeVolumes",
-          "ec2:DescribeVolumesModifications",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeDhcpOptions",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DetachVolume",
-          "ec2:ModifyInstanceAttribute",
-          "ec2:ModifyVolume",
-          "ec2:RevokeSecurityGroupIngress",
-          "ec2:DescribeAccountAttributes",
-          "ec2:DescribeAddresses",
-          "ec2:DescribeInternetGateways",
-          "elasticloadbalancing:AddTags",
-          "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-          "elasticloadbalancing:AttachLoadBalancerToSubnets",
-          "elasticloadbalancing:ConfigureHealthCheck",
-          "elasticloadbalancing:CreateListener",
-          "elasticloadbalancing:CreateLoadBalancer",
-          "elasticloadbalancing:CreateLoadBalancerListeners",
-          "elasticloadbalancing:CreateLoadBalancerPolicy",
-          "elasticloadbalancing:CreateTargetGroup",
-          "elasticloadbalancing:DeleteListener",
-          "elasticloadbalancing:DeleteLoadBalancer",
-          "elasticloadbalancing:DeleteLoadBalancerListeners",
-          "elasticloadbalancing:DeleteTargetGroup",
-          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-          "elasticloadbalancing:DeregisterTargets",
-          "elasticloadbalancing:DescribeListeners",
-          "elasticloadbalancing:DescribeLoadBalancerAttributes",
-          "elasticloadbalancing:DescribeLoadBalancerPolicies",
-          "elasticloadbalancing:DescribeLoadBalancers",
-          "elasticloadbalancing:DescribeTargetGroupAttributes",
-          "elasticloadbalancing:DescribeTargetGroups",
-          "elasticloadbalancing:DescribeTargetHealth",
-          "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-          "elasticloadbalancing:ModifyListener",
-          "elasticloadbalancing:ModifyLoadBalancerAttributes",
-          "elasticloadbalancing:ModifyTargetGroup",
-          "elasticloadbalancing:ModifyTargetGroupAttributes",
-          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-          "elasticloadbalancing:RegisterTargets",
-          "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-          "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-          "kms:DescribeKey"
-        ],
-        Resource: "*"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
       },
-      {
-        Effect: "Allow",
-        Action: "iam:CreateServiceLinkedRole",
-        Resource: "*",
-        Condition: {
-          "StringEquals": {
-            "iam:AWSServiceName": "elasticloadbalancing.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_iam_role" "eksWorkerRole" {
   name = "eksWorkerRole"
-  assume_role_policy = jsonencode({
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Effect: "Allow",
-        Action: [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:UpdateAutoScalingGroup",
-          "ec2:AttachVolume",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:CreateRoute",
-          "ec2:CreateSecurityGroup",
-          "ec2:CreateTags",
-          "ec2:CreateVolume",
-          "ec2:DeleteRoute",
-          "ec2:DeleteSecurityGroup",
-          "ec2:DeleteVolume",
-          "ec2:DescribeInstances",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeVolumes",
-          "ec2:DescribeVolumesModifications",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeDhcpOptions",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DetachVolume",
-          "ec2:ModifyInstanceAttribute",
-          "ec2:ModifyVolume",
-          "ec2:RevokeSecurityGroupIngress",
-          "ec2:DescribeAccountAttributes",
-          "ec2:DescribeAddresses",
-          "ec2:DescribeInternetGateways",
-          "elasticloadbalancing:AddTags",
-          "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-          "elasticloadbalancing:AttachLoadBalancerToSubnets",
-          "elasticloadbalancing:ConfigureHealthCheck",
-          "elasticloadbalancing:CreateListener",
-          "elasticloadbalancing:CreateLoadBalancer",
-          "elasticloadbalancing:CreateLoadBalancerListeners",
-          "elasticloadbalancing:CreateLoadBalancerPolicy",
-          "elasticloadbalancing:CreateTargetGroup",
-          "elasticloadbalancing:DeleteListener",
-          "elasticloadbalancing:DeleteLoadBalancer",
-          "elasticloadbalancing:DeleteLoadBalancerListeners",
-          "elasticloadbalancing:DeleteTargetGroup",
-          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-          "elasticloadbalancing:DeregisterTargets",
-          "elasticloadbalancing:DescribeListeners",
-          "elasticloadbalancing:DescribeLoadBalancerAttributes",
-          "elasticloadbalancing:DescribeLoadBalancerPolicies",
-          "elasticloadbalancing:DescribeLoadBalancers",
-          "elasticloadbalancing:DescribeTargetGroupAttributes",
-          "elasticloadbalancing:DescribeTargetGroups",
-          "elasticloadbalancing:DescribeTargetHealth",
-          "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-          "elasticloadbalancing:ModifyListener",
-          "elasticloadbalancing:ModifyLoadBalancerAttributes",
-          "elasticloadbalancing:ModifyTargetGroup",
-          "elasticloadbalancing:ModifyTargetGroupAttributes",
-          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-          "elasticloadbalancing:RegisterTargets",
-          "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-          "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-          "kms:DescribeKey"
-        ],
-        Resource: "*"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
       },
-      {
-        Effect: "Allow",
-        Action: "iam:CreateServiceLinkedRole",
-        Resource: "*",
-        Condition: {
-          "StringEquals": {
-            "iam:AWSServiceName": "elasticloadbalancing.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
 }
 
-//resource "aws_iam_role_policy_attachment" "Cluster_Policy" {
-//  policy_arn = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
-//  role = aws_iam_role.eksClusterRole.id
-//}
+resource "aws_iam_role_policy_attachment" "Cluster_Policy" {
+  policy_arn = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
+  role = aws_iam_role.eksClusterRole.name
+}
 
-//resource "aws_iam_role_policy_attachment" "Node_Policy" {
-//  policy_arn = data.aws_iam_policy.AmazonNodePolicy.arn
-//  role = aws_iam_role.eksWorkerRole.id
-//}
+resource "aws_iam_role_policy_attachment" "Node_Policy" {
+  policy_arn = data.aws_iam_policy.AmazonNodePolicy.arn
+  role = aws_iam_role.eksWorkerRole.name
+}
 
 resource "aws_iam_role_policy_attachment" "EC2_Policy" {
   policy_arn = data.aws_iam_policy.AmazonEC2CR_RO.arn
-  role = aws_iam_role.eksWorkerRole.id
+  role = aws_iam_role.eksWorkerRole.name
 }
 
 resource "aws_iam_role_policy_attachment" "CNI_Policy" {
   policy_arn = data.aws_iam_policy.AmazonCNI.arn
-  role = aws_iam_role.eksWorkerRole.id
+  role = aws_iam_role.eksWorkerRole.name
 }
