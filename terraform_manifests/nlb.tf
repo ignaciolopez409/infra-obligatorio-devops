b3227503febfe994e450b677f3e97c70b8222d46
Renzo DF <renzo.di.fede@gmail.com>
2021-11-17 01:01:38 -0300

    nginx_resources fix

--- terraform_manifests/nlb.tf
+++ terraform_manifests/nlb.tf
@@ -1,6 +1,3 @@
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.cluster_obligatorio.name
-}
 provider "kubectl" {
   host = aws_eks_cluster.cluster_obligatorio.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster_obligatorio.certificate_authority[0].data)
