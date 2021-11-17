b64f0e91008d0a1334d4096f0f2dcde0c42fccd5
Renzo DF <renzo.di.fede@gmail.com>
2021-11-17 00:17:17 -0300

    nlb

--- terraform_manifests/bucket.tf
+++ terraform_manifests/bucket.tf
@@ -1,1 +1,16 @@
-../environments/develop/bucket.tf
+terraform {
+  backend "s3" {
+    bucket = "infra-obligatorio-states"
+    key = "develop/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
