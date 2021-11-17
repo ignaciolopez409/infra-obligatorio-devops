e5cddcf24c5d495870a6e71b1e3aa0feaa7a00be
root <root@Devops01.geocom.com.uy>
2021-11-16 22:21:56 -0500

    link

--- terraform_manifests/bucket.tf
+++ terraform_manifests/bucket.tf
@@ -1,16 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "infra-obligatorio-states"
-    key = "develop/terraform.tfstate"
-    region = "us-east-1"
-  }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    kubectl = {
-      source = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-}
+../environments/develop/bucket.tf
