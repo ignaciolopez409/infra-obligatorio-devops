1f337f35d165dff2e2552c2790cb0a3ea98090b1
ilopez <ilopez@geocom.com.uy>
2021-11-17 00:25:20 -0300

    test auth

--- terraform_manifests/nlb.tf
+++ terraform_manifests/nlb.tf
@@ -4,7 +4,7 @@ data "aws_eks_cluster_auth" "main" {
 
 provider "kubectl" {
   host = aws_eks_cluster.cluster_obligatorio.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.main.token)
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster_obligatorio.certificate_authority[0].data)
   token = data.aws_eks_cluster_auth.main.token
   load_config_file = false
 }
