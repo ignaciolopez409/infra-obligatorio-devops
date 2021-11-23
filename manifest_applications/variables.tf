6ba5fd826e0280f240d8aeafe4f39978e1e89a0f
Renzo DF <renzo.di.fede@gmail.com>
2021-11-23 00:28:26 -0300

    helm charts initialitez

--- manifest_applications/variables.tf
+++ manifest_applications/variables.tf
@@ -7,3 +7,15 @@ variable "region" {
   type = string
   default = "us-east-1"
 }
+variable "orders_service_version" {
+  type = string
+}
+variable "products_service_version" {
+  type = string
+}
+variable "shipping_service_version" {
+  type = string
+}
+variable "payments_service_version" {
+  type = string
+}
