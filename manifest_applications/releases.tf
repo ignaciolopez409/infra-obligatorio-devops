ea300f428e5ad0d71731dbd48a3c854598cd9af3
Renzo DF <renzo.di.fede@gmail.com>
2021-11-27 20:22:51 -0300

    namespace

--- manifest_applications/releases.tf
+++ manifest_applications/releases.tf
@@ -17,3 +17,47 @@ resource "helm_release" "releases" {
   values = [
     file("./values/${each.key}.yaml")]
 }
+
+resource "kubernetes_ingress" "example_ingress" {
+  metadata {
+    name = "example-ingress"
+    annotations = {
+      "ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+  spec {
+    rule {
+      http {
+        path {
+          path = "/orders"
+          backend {
+            service_name = "orders-service"
+            service_port = "8080"
+          }
+        }
+        path {
+          path = "/payments"
+          backend {
+            service_name = "payments-service"
+            service_port = "8080"
+          }
+        }
+        path {
+          path = "/shipping"
+          backend {
+            service_name = "shipping-service"
+            service_port = "8080"
+          }
+        }
+        path {
+          path = "/products"
+          backend {
+            service_name = "products-service"
+            service_port = "8080"
+          }
+        }
+      }
+    }
+  }
+}
+
