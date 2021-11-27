3eebbfa7f5146c69666e06ee851027fb34616064
Renzo DF <renzo.di.fede@gmail.com>
2021-11-27 20:08:30 -0300

    namespace

--- manifest_applications/nginx_resources.tf
+++ manifest_applications/nginx_resources.tf
@@ -1,5 +1,6 @@
 resource "helm_release" "release-ingress" {
-  chart = "https://kubernetes.github.io/ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
   name = "ingress-nginx"
   create_namespace = true
   namespace = "ingress-nginx"
