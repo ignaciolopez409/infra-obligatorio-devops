d3333bcf6626ffe0adbb64201beba06ac207da98
Renzo DF <renzo.di.fede@gmail.com>
2021-11-27 19:40:33 -0300

    namespace

--- manifest_applications/provider.tf
+++ manifest_applications/provider.tf
@@ -28,5 +28,14 @@ provider "helm" {
   kubernetes {
     host = data.terraform_remote_state.eks_state.outputs.endpoint_out
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks_state.outputs.kubeconfig-certificate-authority-data_out)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        data.terraform_remote_state.eks_state.outputs.eks_cluster_name]
+      command = "aws"
+    }
   }
 }
