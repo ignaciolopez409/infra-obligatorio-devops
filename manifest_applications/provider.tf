143b0179b4eecebf9d7c0636054808eae29adfd1
Renzo DF <renzo.di.fede@gmail.com>
2021-12-09 18:59:19 -0300

    overview-html

--- manifest_applications/provider.tf
+++ manifest_applications/provider.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-data "terraform_remote_state" "eks_state" {
-  backend = "s3"
-  config = {
-    bucket = "${var.ENV}-states"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 provider "kubernetes" {
   //cluster_ca_certificate = base64decode(data.terraform_remote_state.eks_state.outputs.kubeconfig-certificate-authority-data_out)
   config_path = "~/.kube/config"
